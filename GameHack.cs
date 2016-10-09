51b8eebbd3ec15a148e9a88dfce5f917f3f4e812
Sui <link_matrix1@o2.pl>
2016-10-09 03:06:06 +0200

    Got it working

--- GameHack.cs
+++ GameHack.cs
@@ -21,6 +21,8 @@ namespace AVP_CustomLauncher
         int ResolutionX = 1280;
         int ResolutionY = 720;
         float desiredfov = 90.0f;
+        float readBgValue = 0;
+        float bgCorrectedValue = 1.308997035f;
         float ReadFovX = 0;
         float ReadFovY = 0;
         float fovX = 2;
@@ -28,14 +30,19 @@ namespace AVP_CustomLauncher
         bool foundProcess = false;
         bool dllInjected = false;
 
-        int fovAddress = 0x001DDFEC;
+        int fovAddress = 0x01DDFEC;
         int[] offsetFovX = new int[] { 0x4, 0xC4 };
         int[] offsetFovY = new int[] { 0x4, 0xC8 };
+
+        int bgScalingAddress = 0x001E03A4;
+        int[] bgScalingOffsets = new int[] { 0x2BC, 0x1c4 };
 
         public void DoWork()
         {
             fovY = HorizontalFOVToVerticalRadians(desiredfov);
-            fovX = VerticalRadiansToHorizontalRadiansWithResolution(fovY);
+            fovX = VerticalRadiansToHorizontalFor4By3Monitor(fovY);
+            bgCorrectedValue = correntMenuBGWithAspect(1.308997035f);
+
 
             Trace.WriteLine("Display FOV calculated to: " + fovX + " horizontal, " + fovY + " vertical");
             System.Threading.Thread.Sleep(5000);
@@ -73,17 +80,28 @@ namespace AVP_CustomLauncher
                     {
                         ReadFovX = Trainer.ReadPointerFloat(myProcess, cshellBaseAdress + fovAddress, offsetFovX);
                         ReadFovY = Trainer.ReadPointerFloat(myProcess, cshellBaseAdress + fovAddress, offsetFovY);
-                        
+                        readBgValue = Trainer.ReadPointerFloat(myProcess, cshellBaseAdress + bgScalingAddress, bgScalingOffsets);
+
+
+
                         if (ReadFovX != fovX && ReadFovX != 0x0000000)
                             Trainer.WritePointerFloat(myProcess, cshellBaseAdress + fovAddress, offsetFovX, fovX);
                         if (ReadFovY != fovY && ReadFovY != 0x0000000)
                             Trainer.WritePointerFloat(myProcess, cshellBaseAdress + fovAddress, offsetFovY, fovY);
+                        if (readBgValue != bgCorrectedValue && readBgValue != 0x0000000)
+                            Trainer.WritePointerFloat(myProcess, cshellBaseAdress + bgScalingAddress, bgScalingOffsets, bgCorrectedValue);
 
-                        if(!dllInjected && LithTechBaseAdress != 0x0 && cshellBaseAdress != 0x0)
+                        if (!dllInjected && LithTechBaseAdress != 0x0 && cshellBaseAdress != 0x0)
                         {
                             DllInjectionResult result = DllInjector.GetInstance.Inject(myProcess, "widescreenfix.dll");
                             Debug.WriteLine("DLL Injection: " + result);
-                            dllInjected = true;
+                            if(result == DllInjectionResult.Success)
+                            {
+                                dllInjected = true;
+                            }
+
+
+                            System.Threading.Thread.Sleep(500);
 
                             ProcessModuleCollection modules = myProcess[0].Modules;
                             ProcessModule dllBaseAdressIWant = null;
@@ -95,9 +113,21 @@ namespace AVP_CustomLauncher
                                 }
                             }
                             hookedDllAddress = dllBaseAdressIWant.BaseAddress.ToInt32();
-                            Debug.WriteLine("DLL Injected at: 0x" + hookedDllAddress.ToString("X4"));
-                            Trainer.WriteInteger(myProcess, hookedDllAddress + 0x19000, ResolutionX);
-                            Trainer.WriteInteger(myProcess, hookedDllAddress + 0x19004, ResolutionY);
+                            
+                            #if DEBUG
+                            {
+                                Debug.WriteLine("DLL Injected at: 0x" + hookedDllAddress.ToString("X4"));
+                                Trainer.WriteInteger(myProcess, hookedDllAddress + 0x19008, ResolutionX);
+                                Trainer.WriteInteger(myProcess, hookedDllAddress + 0x1900C, ResolutionY);
+                                Debug.WriteLine("Written to dllHook: " + ResolutionX + "x" + ResolutionY + " at address " + (hookedDllAddress + 0x19008).ToString("X4"));
+                            }
+                            #else
+                            {
+                                //And HOPE NOTHING MOVED
+                                Trainer.WriteInteger(myProcess, hookedDllAddress + 0x4008, ResolutionX);
+                                Trainer.WriteInteger(myProcess, hookedDllAddress + 0x400C, ResolutionY);
+                            }
+                            #endif
                         }
                     }
                     System.Threading.Thread.Sleep(threadDelay);
@@ -110,11 +140,19 @@ namespace AVP_CustomLauncher
             }
         }
 
+        private float correntMenuBGWithAspect(float bgVal)
+        {
+            double verticalVal = 2 * Math.Atan(Math.Tan(bgVal / 2) * (ResolutionY * 1.0 / ResolutionX * 1.0));
+            double dHorizontalRadians = 2 * Math.Atan(Math.Tan(verticalVal / 2) * (1024 * 1.0 / 768 * 1.0));
+            return Convert.ToSingle(dHorizontalRadians);
+        }
+
         public void SendValues(float value, int ResX, int ResY)
         {
             desiredfov = value;
             ResolutionX = ResX;
             ResolutionY = ResY;
+            Debug.WriteLine("Thread received values: " + ResolutionX + "x" + ResolutionY + " @ " + desiredfov);
         }
         
         public void RequestStop()
@@ -130,6 +168,18 @@ namespace AVP_CustomLauncher
             return Math.PI * angle / 180.0;
         }
 
+        public float VerticalRadiansToHorizontalFor4By3Monitor(float angle)
+        {
+            double dVerticalRadians = Convert.ToDouble(angle);
+            double dHorizontalRadians;
+
+            dHorizontalRadians = 2 * Math.Atan(Math.Tan(dVerticalRadians / 2) * (1024 * 1.0 / 768 * 1.0));
+
+            return Convert.ToSingle(Math.Round(dHorizontalRadians, 3));
+        }
+
+        /*
+        This part is no longer used, as the calculation is done using injected DLL
         public float VerticalRadiansToHorizontalRadiansWithResolution(float angle)
         {
             double dVerticalRadians = Convert.ToDouble(angle);
@@ -138,7 +188,7 @@ namespace AVP_CustomLauncher
             dHorizontalRadians = 2 * Math.Atan(Math.Tan(dVerticalRadians / 2) * (ResolutionX * 1.0 / ResolutionY * 1.0));
 
             return Convert.ToSingle(Math.Round(dHorizontalRadians, 3));
-        }
+        }*/
 
         public float HorizontalFOVToVerticalRadians(float angle)
         {
--- widescreenfix/Main.cpp
+++ widescreenfix/Main.cpp
@@ -1,11 +1,16 @@
 #include <Windows.h>
 #include "Functions.h"
 #include "Main.h"
+#include <math.h>
 
 static DWORD cshellAddress = 0x0;
-//static float aspectRatio = 1.333;
+static DWORD baseAddress = (DWORD)0x00400000;
+static float aspectRatio = 1.337f;
 static DWORD resolutionX = 1024;
 static DWORD resolutionY = 768;
+static DWORD verticalRadians = 0;
+static DWORD horizontalRadians = 0;
+
 
 bool Hook(void * toHook, void * ourFunction, int lenght)
 {
@@ -27,14 +32,57 @@ bool Hook(void * toHook, void * ourFunction, int lenght)
 
 DWORD jmpResAddress;
 DWORD jmpFovAddress;
+float horizontalRadiansF;
+
+static float increaseHorFOV()
+{
+	float tempVradian = 2 * atanf(tanf(horizontalRadiansF / 2.0f) * 0.75f);
+	return (2.0f * atanf(tanf(tempVradian / 2.0f) * aspectRatio));
+}
 
 void __declspec(naked) fovHack()
 {
-	//DisplayFOV
-	//Start at: d3d.ren+FA52
-	//Ends at: d3d.ren+FA71
-	//TODO: Write the corrected FOV function
+	/*
+	lithtech.exe+B7E1 - 25 00410000           - and eax,00004100 { 16640 }
+	lithtech.exe+B7E6 - 75 08                 - jne lithtech.exe+B7F0
+	lithtech.exe+B7E8 - DDD8                  - fstp st(0)
+	lithtech.exe+B7EA - D9 05 186B4C00        - fld dword ptr [lithtech.exe+C6B18] { [6.25] }
+	lithtech.exe+B7F0 - 8B 44 24 08           - mov eax,[esp+08]
+	lithtech.exe+B7F4 - D9 99 C4010000        - fstp dword ptr [ecx+000001C4]
+	lithtech.exe+B7FA - 89 81 C0010000        - mov [ecx+000001C0],eax
+	lithtech.exe+B800 - C3                    - ret {to cshell.dll +602B8}
+	*/
 
+	__asm
+	{
+		fstp dword ptr[ecx + 0x000001C4]
+		push ebx
+		push ecx
+		push edx
+		push esi
+		push edi
+		push esp
+		push ebp
+		mov horizontalRadiansF, eax
+	}
+	horizontalRadiansF = increaseHorFOV();
+
+	__asm
+	{
+		pop ebp
+		pop esp
+		pop edi
+		pop esi
+		pop edx
+		pop ecx
+		pop ebx
+		mov eax, horizontalRadiansF
+		mov[ecx + 0x000001C0], eax
+		jmp[jmpFovAddress]
+	}
+}
+void __declspec(naked) resHack()
+{
 	__asm
 	{
 		mov eax, resolutionX
@@ -42,46 +90,6 @@ void __declspec(naked) fovHack()
 		jmp[jmpResAddress]
 	}
 }
-
-void __declspec(naked) resHack()
-{
-	__asm 
-	{
-		mov eax, resolutionX
-		mov ecx, resolutionY
-		jmp [jmpResAddress]
-	}
-}
-
-bool rendererHooked = false;
-DWORD rendererModuleAddress = 0x0;
-void periodicCheck()
-{
-	DWORD address = GetBaseAddressForModuleInfo("d3d.ren");
-	if (address != rendererModuleAddress)
-	{
-		//I have no idea why I wrote this like this...
-		rendererModuleAddress = address;
-
-		//This I know however - base address is 0, it means the d3d.ren got deallocated, needs to be rehooked.
-		if (address == 0x0)
-			rendererHooked = false;
-		else
-		{
-			//Module was found, if it needs to be rehooked - do that. Set flag to true either way
-			if (!rendererHooked)
-			{
-				int hookLenght = 0x5;
-				DWORD hookAddress = rendererModuleAddress + 0xFA52;
-				Hook((void*)hookAddress, fovHack, hookLenght);
-			}
-
-			rendererHooked = true;
-		}
-	}
-}
-
-
 
 DWORD WINAPI HookThread(LPVOID param)
 {
@@ -99,9 +107,21 @@ DWORD WINAPI HookThread(LPVOID param)
 		Hook((void*)hookAddress, resHack, hookLenght);
 	}
 
+	//FOV hack
+	{
+		//Overriding:
+		//fstp dword ptr [ecx+000001C4]	- 6 OP bytes
+		//mov [ecx+000001C0],eax		- 6 OP bytes
+		int hookLenght = 12;
+		DWORD hookAddress = baseAddress + 0xB7F4; 		//Solve address = "lithtech.exe"+B7F4
+		jmpFovAddress = hookAddress + hookLenght;
+		//Hook((void*)hookAddress, fovHack, hookLenght);
+		Hook((void*)hookAddress, fovHack, hookLenght);
+	}
+
 	while (true)
 	{
-		periodicCheck();
+		aspectRatio = (float)(1.0 * resolutionX / resolutionY);
 		Sleep(400);
 	}
 }
